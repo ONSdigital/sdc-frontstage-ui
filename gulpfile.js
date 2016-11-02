1779efc89d6e27b3db8eb5d149d29cc662925804
Shane Edwards <shane.edwards9@gmail.com>
2016-11-02 11:57:16 +0000

    Added login and register user journies HTML

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	concat = require('gulp-concat'),
 	livereload = require('gulp-livereload'),
+	mustache = require('gulp-mustache'),
 
 	config = {
 		sassSrc: './sass/*.scss',
+		templatesSrc: ['./templates/**/*.mustache', './mock-pages/**/*.html'],
 		outputDir: './dist'
 	};
 
+
+gulp.task('compile:templates', () => {
+	return gulp.src('./mock-pages/src/**/*.html')
+		.pipe(mustache({
+			title: 'Log in to your account'
+		}))
+		.pipe(gulp.dest('./mock-pages/dist'));
+});
 
 gulp.task('compile:sass', () => {
 	return gulp.src(config.sassSrc)
@@ -17,6 +27,10 @@ gulp.task('compile:sass', () => {
 		.pipe(livereload());
 });
 
+gulp.task('watch:templates', () => {
+	gulp.watch(config.templatesSrc, ['compile:templates']);
+});
+
 gulp.task('watch:compile:sass', ['compile:sass'], () => {
 	gulp.watch(config.sassSrc, ['compile:sass']);
 });
@@ -24,7 +38,10 @@ gulp.task('watch:compile:sass', ['compile:sass'], () => {
 
 
 gulp.task('dev', [
-	'watch:compile:sass'
+	'compile:sass',
+	'compile:templates',
+	'watch:compile:sass',
+	'watch:templates'
 ], () => {
 	livereload.listen();
 });
