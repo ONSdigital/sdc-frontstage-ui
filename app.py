c7cab8fbbac1df8d316903ba3e66bb3d1634d585
Nick Gravgaard <me@nickgravgaard.com>
2016-11-08 16:28:42 +0000

    Add HTML from mock-pages to my-surveys template

--- app.py
+++ app.py
@@ -39,6 +39,15 @@ def my_surveys():
     return render_mustache('my-surveys', surveys=surveys)
 
 
+# quick hack to serve the CSS file
+@app.route('/dist/site.min.css', methods=['GET'])
+def css_endpoint() -> Response:
+    filename = 'dist/site.min.css'
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+    with open(path, 'r') as f:
+        return Response(f.read(), mimetype='text/css')
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
